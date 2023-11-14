71ea275008fac835f4732aca756b7cda68edacd4
melda-gonen-vinci <melda.gonen@student.vinci.be>
2023-11-14 15:42:05 +0100

    feat: greeting multiple names with upper and lower case

--- greeting.js
+++ greeting.js
@@ -18,19 +18,46 @@ function greetingWithNoName () {
 }
 
 function greetingWithUpperCase (name) {
-  return `HELLO, ${name}.`
+  return `HELLO, ${name}!`
 }
 
-function greetingWithMultipleNames (name) {
-  const index = name.length - 1
+function greetingWithMultipleNames (names) {
+  const normalNames = []
+  const upperCaseNames = []
 
-  return (
-    'Hello, ' +
-        name.slice(0, index).join(', ') +
-        ' and ' +
-        name[index] +
-        '.'
-  )
+  names.forEach((name) => {
+    if (name === name.toUpperCase()) {
+      upperCaseNames.push(name)
+    } else {
+      normalNames.push(name)
+    }
+  })
+
+  const index = normalNames.length - 1
+  const index2 = upperCaseNames.length - 1
+
+  let greeting = ''
+
+  if (normalNames.length > 0 && upperCaseNames.length === 0) {
+    greeting +=
+            'Hello, ' +
+            normalNames.slice(0, index).join(', ') +
+            ' and ' +
+            normalNames[index] +
+            '.'
+  } else if (upperCaseNames.length > 0 && normalNames.length > 0) {
+    greeting +=
+            'Hello, ' +
+            normalNames.slice(0, index).join(', ') +
+            ' and ' +
+            normalNames[index] +
+            '. AND ' +
+            'HELLO, ' +
+            upperCaseNames.slice(0, index2).join(', ') +
+            upperCaseNames[index2] +
+            '!'
+  }
+  return greeting
 }
 
 module.exports = greeting
