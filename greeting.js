6d5901f2e482f6f81cdb09047ef1816e79540ec1
melda-gonen-vinci <melda.gonen@student.vinci.be>
2023-11-14 12:11:53 +0100

    feat: greeting with null, undefined or empty instead of name

--- greeting.js
+++ greeting.js
@@ -1,5 +1,12 @@
 function greeting (name) {
+  if (name === null || name === undefined || name === '') {
+    return greetingWithNoName()
+  }
   return `Hello, ${name}.`
+}
+
+function greetingWithNoName () {
+  return 'Hello, my friend.'
 }
 
 module.exports = greeting
